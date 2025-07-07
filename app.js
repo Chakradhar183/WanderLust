4c721e69dd9311b63a238263296e3896a17dd581
Chakradhar183 <sethuchakradhar.s001@gmail.com>
2025-07-07 19:38:27 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -54,11 +54,12 @@ let sessionOptions={
   secret:process.env.SECRET,
    resave:false,
    saveUninitialized:true,
-   cookie:{
-    expires:Date.now()+7*24*60*60*1000,
-    maxAge:7*24*60*60*1000,
-    httpOnly:true,
-   }
+   cookie: {
+  expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // ✅
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+}
+
 };
 
 
